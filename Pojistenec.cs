69b5346e4d26da2c929041c32582dc1f8ba45e9b
Luky <Luky@DESKTOP-BE9JAF2>
2024-07-04 21:19:09 +0200

    Add project files.

---
+++ Pojistenec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidenceBasic;
+
+internal class Pojistenec
+{
+    public string Jmeno { get; private set; }
+    public string Prijmeni { get; private set; }
+    public int Vek { get; private set; }
+    public int Telefon { get; private set; }
+
+    public Pojistenec(string jmeno, string prijmeni, int vek, int telefon)
+    {
+        Jmeno = jmeno;
+        Prijmeni = prijmeni;
+        Vek = vek;
+        Telefon = telefon;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("{0,-15}{1,-15}{2,-5}{3,-15}", Jmeno, Prijmeni, Vek, Telefon);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace EvidenceBasic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Obsluha.Evidence();
+    }
+}
